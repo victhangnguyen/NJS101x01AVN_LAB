8bd2949b6452f9478d2155d3766a50540140d2c2
victhangnguyen <victhangnguyen@outlook.com>
2022-08-13 16:57:58 +0700

    Routing Requests - Chuyển hướng Requests

--- app.ts
+++ app.ts
@@ -1,25 +1,38 @@
-  // const http = require('http'); //! NodeRequire
-  import http, { IncomingMessage, ServerResponse } from 'http';
-  //! type RequestListener = (req: IncomingMessage, res: ServerResponse) => void
-  function reqListener(req: IncomingMessage, res: ServerResponse): void {
-    //! req: contain data about request
-    //! res: help you send a response
-    console.log('req.url: ', req.url);
-    console.log('req.method: ', req.method);
-    console.log('req.headers: ', req.headers);
-    // process.exit();
-    res.setHeader('Content-Type', 'text/html');
-    //! write some data to the Response
+// const http = require('http'); //! NodeRequire
+import http, { IncomingMessage, ServerResponse } from 'http';
+//! type RequestListener = (req: IncomingMessage, res: ServerResponse) => any
+function reqListener(req: IncomingMessage, res: ServerResponse): any {
+  const url = req.url;
+  console.log('req.url: ', url);
+  console.log('req.method: ', req.method);
+  console.log('req.headers: ', req.headers);
+
+  if (url === '/') {
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.JS Server</h1></body>');
+    res.write(`
+      <body>
+        <form action="/message" method="POST" novalidate>
+          <input type="text" name="message">
+          <button type="submit">Send</button>
+        </form>
+      </body>
+    `);
     res.write('</head>');
-    //! we need to tell Node, we're done with creating that response
-    res.end(); //! nodejs will send it back to the Client
+    return res.end(); //! reqListener renturn : any
+    //! return to quit the function execution.
   }
 
-  const server = http.createServer(reqListener);
-  //! reqListenter is executed for every incomming request
-  //! where tell Node if X (toched server) happens, then do Y (involk this callback)
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.JS Server</h1></body>');
+  res.write('</head>');
+  res.end(); //! nodejs will send it back to the Client
+}
 
-  server.listen(3000);
+const server = http.createServer(reqListener);
+//! reqListenter is executed for every incomming request
+//! where tell Node if X (toched server) happens, then do Y (involk this callback)
+
+server.listen(3000);
