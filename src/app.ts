5630ba28d71b1b16687cf229ad72266ca8f4f551
victhangnguyen <victhangnguyen@outlook.com>
2022-08-14 23:23:32 +0700

    Parsing Request Bodies - Đọc body của request

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // const http = require('http'); //! NodeRequire
 import http, { IncomingMessage, ServerResponse } from 'http';
 import fs from 'fs';
+import Logging from './library/Logging';
 
 //! type RequestListener = (req: IncomingMessage, res: ServerResponse) => any
 function reqListener(req: IncomingMessage, res: ServerResponse): any {
@@ -25,13 +26,35 @@ function reqListener(req: IncomingMessage, res: ServerResponse): any {
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'Message Content');
+    const body: any[] = [];
+    //! second arg which is that function that should be executed for every incomingData event (giống reqListener).
+    //! on allow us to listen to certain events
+    //! Register Event Listen for the data event whenever a new chunk is ready.
+    req.on('data', (chunk) => {
+      //! this listener receives a Chunk of data.
+      body.push(chunk);
+      Logging.info(chunk);
+      //! Nodejs will execute this, so often until it's done getting all of data out of our request.
+    });
+    //! register Event Listener on End
+    //! data     end
+    //! [][][][]|[][][][]
+    //!          Buffer
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      //! toString input with type=text, because the body of that request will be text => incoming data is text
+      Logging.info(parsedBody); //! outputs: message=InputContent
+      const message = parsedBody.split('=')[1];
+      //! req.on('end', ()=>{}) is just register a function to be called in the future, it's not executed immediately
+      fs.writeFileSync('message.txt', message); //! move it into the Event Listener, if this statement depends on the incoming data.
+    });
+
     //! write some meta information
     //! 302 stands for redirection
     // res.writeHead(302, )
     res.statusCode = 302;
     //! / will automatically use the host
-    res.setHeader('Location', '/alo');
+    res.setHeader('Location', '/');
     return res.end();
   }
 
