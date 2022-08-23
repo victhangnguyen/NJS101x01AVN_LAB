233e80f96f2dc9fe9f386133a45ff83f7f00d4b3
victhangnguyen <victhangnguyen@outlook.com>
2022-08-23 23:38:20 +0700

    Adding Middleware - Cài đặt Middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,24 @@
-import http from 'http';
-import Logging from './library/Logging';
-import express from 'express';
+  import http from 'http';
+  import Logging from './library/Logging';
+  import express, { Request, Response, NextFunction } from 'express';
 
-//! an instance of the app object
-const app = express();
-//! type RequestListener = (req: IncomingMessage, res: ServerResponse) => any
-const server = http.createServer();
+  //! an instance of the app object
+  const app = express();
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log('In the middleware 1!');
+    next(); //! This allows the Request to continue to the Next Middleware in line (funnel)
+  });
+  //! use allow us to add a new Middleware function, it accespt an array of so-called RequestHandler
+  //! function will be executed for every Icomming Request
+  //! next allow request to travel on to the next middleware
 
-server.listen(3000);
+  //! localhost:3000, the browser keep on spinning => we dont get a Response
+  //! this allow us to HOOK into this funnel through which the Request is sent.
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log('In the middleware 2!');
+  });
+
+  const server = http.createServer(app);
+
+  server.listen(3000);
