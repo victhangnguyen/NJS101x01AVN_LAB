a100bc78a52c24961936444b05dc2ac082b35637
victhangnguyen <victhangnguyen@outlook.com>
2022-08-31 01:08:52 +0700

    Giới hạn việc thực thi Middleware đối với Post Requests

--- dist/app.js
+++ dist/app.js
@@ -9,11 +9,6 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 //! Register Middlewares
 app.use(express_1.default.urlencoded({ extended: false }));
-app.use('/product', (req, res, next) => {
-    Logging_1.default.info(req.body);
-    //! Redirect
-    res.redirect('/');
-});
 app.use('/add-product', (req, res, next) => {
     res.send(`
     <form action="/product" method="POST">
@@ -23,10 +18,11 @@ app.use('/add-product', (req, res, next) => {
     </form>
   `);
 });
-// next(); //! The Request goes through the File from TOP to BOTTOM,
-//! if we dont call next(); it's not going to the next() middleware.
-//! If middlewares 1 don't call next(), this middleware 2 will never get a chance of handling that Request.
-//!
+app.post('/product', (req, res, next) => {
+    Logging_1.default.info(req.body);
+    //! Redirect
+    res.redirect('/');
+});
 app.use('/', (req, res, next) => {
     //! (property) Response<any, Record<string, any>, number>.send: (body?: any) => express.Response<any, Record<string, any>>
     res.send(`<h1>Hello from Express</h1>`);
--- src/app.ts
+++ src/app.ts
@@ -12,12 +12,6 @@ const app = express();
 //! Register Middlewares
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/product', (req: Request, res: Response, next: NextFunction) => {
-  Logging.info(req.body);
-  //! Redirect
-  res.redirect('/');
-});
-
 app.use('/add-product', (req: Request, res: Response, next: NextFunction) => {
   res.send(`
     <form action="/product" method="POST">
@@ -27,11 +21,13 @@ app.use('/add-product', (req: Request, res: Response, next: NextFunction) => {
     </form>
   `);
 });
-// next(); //! The Request goes through the File from TOP to BOTTOM,
-//! if we dont call next(); it's not going to the next() middleware.
 
-//! If middlewares 1 don't call next(), this middleware 2 will never get a chance of handling that Request.
-//!
+app.post('/product', (req: Request, res: Response, next: NextFunction) => {
+  Logging.info(req.body);
+  //! Redirect
+  res.redirect('/');
+});
+
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   //! (property) Response<any, Record<string, any>, number>.send: (body?: any) => express.Response<any, Record<string, any>>
   res.send(`<h1>Hello from Express</h1>`);
