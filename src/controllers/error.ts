33ece87d5343d370e0a2327851f5976bcfddcfce
Vic Thang Nguyen <victhangnguyen@outlook.com>
2022-10-16 22:27:32 +0700

    Understanding Validation

--- src/controllers/error.ts
+++
@@ -1,9 +0,0 @@
-import { RequestHandler } from 'express';
-
-//! exports.get404()
-export const get404: RequestHandler = (req, res, next) => {
-  res.render('404', {
-    path: '',
-    pageTitle: 'Page Not Found',
-  });
-};
--- src/utils/path.ts
+++
@@ -1,3 +0,0 @@
-import path from 'path';
-
-export default path.dirname(require.main?.filename as string);
