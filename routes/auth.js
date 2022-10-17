83e101384451ed0a5763a199b292ea076a5efcce
Vic Thang Nguyen <victhangnguyen@outlook.com>
2022-10-18 02:13:30 +0700

    Lab 9.6: Thêm kiểm tra dữ liệu không đồng bộ - Adding Async Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { check, body } = require('express-validator');
 
+//! imp models
+const User = require('../models/user');
+
 const authController = require('../controllers/auth');
 
 const router = express.Router();
@@ -18,10 +21,17 @@ router.post(
       .isEmail()
       .withMessage('Please enter a valid email.')
       .custom((value, { req }) => {
-        if (value === 'test@test.com') {
-          throw new Error('This email address if forbidden.');
-        }
-        return true;
+        // if (value === 'test@test.com') {
+        //   throw new Error('This email address if forbidden.');
+        // }
+        // return true;
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              'E-Mail exists already, please pick a different one.'
+            );
+          }
+        });
       }),
     body(
       'password',
