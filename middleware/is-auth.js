9514fcb8b3e2b248296143c495f3edd5da61f1da
Vic Thang Nguyen <victhangnguyen@outlook.com>
2022-10-20 00:49:42 +0700

    Lab 9.19: Mã trạng thái - Status Code

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,6 +1,9 @@
 module.exports = (req, res, next) => {
-    if (!req.session.isLoggedIn) {
-        return res.redirect('/login');
-    }
-    next();
-}
+  if (!req.session.isLoggedIn) {
+    // return res.status(401).redirect('/login');
+    //! I'm sending a 300 status code but of course we could add status 401 here to kind of also make clear what the problem
+    //! but it will be overwritten with a 300 code due to redirect.
+    return res.redirect('/login');
+  }
+  next();
+};
