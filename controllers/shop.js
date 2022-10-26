696b2dca4d0bf45d610882459ba981b030289f9a
Vic Thang Nguyen <victhangnguyen@outlook.com>
2022-10-26 18:15:11 +0700

    Lab 10.19: Bỏ qua & Giới hạn dữ liệu với SQL - Skip & Limit with SQL

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,6 +41,17 @@ exports.getProduct = (req, res, next) => {
     });
 };
 
+// Skip & Limit with SQL
+// When using MongoDB, you can use skip() and limit() as shown in the last lecture.
+
+// But how would that work in SQL?
+
+// Here's how you would implement pagination in SQL code: https://stackoverflow.com/questions/3799193/mysql-data-best-way-to-implement-paging
+
+// To quickly sum it up: The LIMIT command allows you to restrict the amount of data points you fetch, it's your limit() equivalent. Combined with the OFFSET command (which replaces skip()), you can control how many items you want to fetch and how many you want to skip.
+
+// When using Sequelize, the official docs describe how to add pagination: https://sequelize.org/master/manual/model-querying-basics.html
+
 exports.getIndex = (req, res, next) => {
   const page = req.query.page;
   Product.find()
@@ -205,28 +216,6 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.fontSize(20).text(`Total Price: $${totalPrice}`);
 
       pdfDoc.end();
-
-      // fs.readFile(invoicePath, (err, dataBuffer) => {
-      //   if (err) {
-      //     return next(err);
-      //   }
-
-      //   res.setHeader('Content-Type', 'application/pdf');
-      //   //! This allow us to define How this content should be served to the Cliend (inline or attachment)
-      //   res.setHeader(
-      //     'Content-Disposition',
-      //     'inline; filename="' + invoiceName + '"'
-      //   );
-
-      //   res.send(dataBuffer);
-      // });
-
-      // function createReadStream(path: fs.PathLike, options?: BufferEncoding | ReadStreamOptions | undefined): fs.ReadStream (+1 overload)
-      // const file = fs.createReadStream(invoicePath);
-
-      // file.pipe(res);
-      //! use pipe to forward the data that is read in with that stream to my response because the response obj is a writableStream acutally.
-      //! not every object is a wriableStream but the response happens to be one.
     })
     .catch((err) => {
       next(err);
